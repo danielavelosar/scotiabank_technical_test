9f9a787e6cd8e29c8e0ea82f9a1aaaacb2e3a4b4
pvelosa <paula.velosa2704@gmail.com>
2024-11-14 02:07:46 +0100

    spring boot initializer

---
+++ scotiabank-test/src/main/java/com/scotiabank/scotiabank_test/ScotiabankTestApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank.scotiabank_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotiabankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotiabankTestApplication.class, args);
+	}
+
+}
